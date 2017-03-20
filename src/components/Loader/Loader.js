cfc19593a4c601b31880e5d511a79010b82a8546
Lenny Peters <lpeters@hollandamericagroup.com>
2017-03-20 09:24:12 -0700

    Master - changes to the loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import loaderfront from '../../svg/loaderfront.svg';
 
 class Loader extends Component {
+    //Create a loader while all of the stuff is going on.
     render() {
         return <div className="loader">
-            <h1>Loading...</h1>
+            <h1>Welcome to DarkSky Weather API</h1>
+            <p>Loading...</p>
             <img src={loaderfront} className="loader-pulse" alt="Loading..." />
         </div>;
     }

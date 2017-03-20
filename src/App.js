5fffc5b2b86444a5911586012cb30de8c1c19ca2
Lenny Peters <lpeters@hollandamericagroup.com>
2017-03-19 19:14:33 -0700

    Master - starting point

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// API Key c00940ee338b6857bc61cf9e4f67815c
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
